f2c528c11e5f54094af50e5946edab78ddd1cbf9
Sakshi Gupta <sarthakgupta5116@gmail.com>
2026-03-07 16:45:03 +0530

    feat: new features added

---
+++ public/sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'milkar-v1';
+const assets = ['/'];
+
+// Install service worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Cache and return requests
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
