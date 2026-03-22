a8a205008b4a941bfcb5bed67f6e420281dd5dd5
Sakshi Gupta <sarthakgupta5116@gmail.com>
2026-03-22 10:20:52 +0530

    feat: new features added

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App.jsx' // Make sure 'App' matches the file name 'App.jsx' exactly
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
